abe77f853e265526df49e86bc92cc47862b2204a
ntloic3tbt@gmail.com <ntloic3tbt@gmail.com>
2023-04-29 09:13:52 +0000

    Feature/Add new jinja

---
+++ models/marts/core/int_orders__pivoted.sql
@@ -0,0 +1,19 @@
+{%- set payment_methods = ['bank_transfer', 'credit_card', 'coupon', 'gift_card'] -%}
+
+with payments as (
+    select * from {{ ref('stg_payments') }}
+),
+
+final as (
+    select
+        order_id,
+        {% for method in payment_methods -%}
+        sum(case when payment_method = '{{method}}' then amount else 0 end) as {{ method }}_amount
+        {%- if not loop.last -%}
+        ,
+        {% endif -%}
+        {%- endfor %}
+    from payments
+)
+
+select * from final;
